db36887cad068208dcd3324066a9885ebbc8c5c4
capcool <sudipta15ee@gmail.com>
2023-06-01 15:51:57 +0530

    htmlchanges

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
-    <div>
+    <div className='text-red-600'>
       hello, we are coming soon !!
     </div>
     </>
