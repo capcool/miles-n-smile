d314f2a4a12bbe782fe0c55a881ae965a1c7e64c
capcool <sudipta15ee@gmail.com>
2023-06-04 16:02:37 +0530

    webpage basic structure

--- app/components/home.js
+++ app/components/home.js
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
       <>
-      <div className='text-blue-600'>
+      <div className='text-blue-600 top-5 p-5'>
         Miles-N-Smiles - Home - Coming soon
       </div>
       </>
--- app/components/sidebar.js
+++ app/components/sidebar.js
@@ -1,11 +1,31 @@
-import {BsArrowLeftShort} from 'react-icons/bs'
+"use client"
+import { BsArrowLeftShort } from 'react-icons/bs'
+import { AiFillEnvironment } from 'react-icons/ai';
+import { useState } from 'react';
 export default function SideBar() {
+    const [open, setOpen] = useState(true);
     return (
-      <>
-      <div className='bg-violet-950 h-screen p-5 pt-8 w-72 text-white relative'>
-        <BsArrowLeftShort className='bg-white text-violet-950 rounded-full text-3xl absolute -right-3 top-9 
-        border border-violet-950 cursor-pointer'/>
-      </div>
-      </>
+        <>
+            <div className={`bg-violet-950 h-screen p-5 pt-8 
+            ${open ? `w-72 ` : 'w-20 '}
+       duration-500 relative`}
+            >
+                <BsArrowLeftShort 
+                className={`bg-white text-violet-950 rounded-full text-3xl absolute
+                -right-3 top-9 border border-violet-950 cursor-pointer 
+                ${!open && 'rotate-180'} `} 
+                onClick={() => setOpen(!open)} 
+                />
+                <div className='inline-flex'>
+                    <AiFillEnvironment className='bg-amber-300 text-4xl rounded 
+                    cursor-pointer float-left mr-2'/>
+                    <h6 className={` text-white origin-left
+                    ${!open && 'scale-0'}`}>
+                        Miles-N-Slimes
+                    </h6>
+                </div>
+            </div>
+
+        </>
     )
-  }
+}
