1c47019202efd80690800a46e298f38652d1722f
capcool <sudipta15ee@gmail.com>
2023-06-04 18:47:28 +0530

    side menu items added

---
+++ app/components/menuitems.js
@@ -0,0 +1,12 @@
+
+import {AiOutlineHome , AiOutlineFire , AiOutlineCar, AiOutlineContacts}  from 'react-icons/ai';
+import {BsAirplane} from 'react-icons/bs';
+const sideMenuItems=[
+{title:"Home",icon:<AiOutlineHome/>},
+{title:"Tregding",icon:<AiOutlineFire/>},
+{title:"Weekend-Getway",icon:<AiOutlineCar/>},
+{title:"Long-Holidays",icon:<BsAirplane/>},
+{title:"Contact Us",icon:<AiOutlineContacts />}
+]
+
+export default sideMenuItems;
--- app/components/sidebar.js
+++ app/components/sidebar.js
@@ -1,9 +1,12 @@
 "use client"
 import { BsArrowLeftShort } from 'react-icons/bs'
 import { AiFillEnvironment } from 'react-icons/ai';
+import sideMenuItems from './menuitems';
 import { useState } from 'react';
 export default function SideBar() {
+   
     const [open, setOpen] = useState(true);
+        
     return (
         <>
             <div className={`bg-violet-950 h-screen p-5 pt-8 
@@ -12,18 +15,36 @@ export default function SideBar() {
             >
                 <BsArrowLeftShort 
                 className={`bg-white text-violet-950 rounded-full text-3xl absolute
-                -right-3 top-9 border border-violet-950 cursor-pointer 
+                -right-3 top-9 border border-violet-950 cursor-pointer duration-300
                 ${!open && 'rotate-180'} `} 
                 onClick={() => setOpen(!open)} 
                 />
                 <div className='inline-flex'>
-                    <AiFillEnvironment className='bg-amber-300 text-3xl rounded block
-                    cursor-pointer float-left mr-2 min-w-[30px]' />
+                    <AiFillEnvironment className={`bg-amber-300 text-3xl rounded block
+                    cursor-pointer float-left mr-2 min-w-[30px] duration-300  ${open && 'rotate-[360deg]'}`} />
                     <h6 className={` text-white origin-left
                     ${!open && 'scale-0'}`}>
                         Miles-N-Slimes
                     </h6>
                 </div>
+                <ul className='pt-2'>
+                    {sideMenuItems.map((menu , index)=>{
+                       return(
+                        <>
+                        <li key={index} className='text-gray-300 text-sm 
+                        flex items-center gap-x-4 cursor-pointer p-2
+                        hover:bg-gray-500 rounded-md mt-2'>
+                            <span className='text-2xl block float-left'>
+                                {menu.icon}</span>
+                            <span className={`text-base  flex-1 
+                            ${!open && 'hidden'}`}>
+                                {menu.title}</span>
+                            
+                        </li>
+                        </>
+                       )
+                    })}
+                </ul>
             </div>
 
         </>
